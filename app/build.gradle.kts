584ef61ea2b4d5138a46786d371d25e77302e11b
rizkigemas <allofmuhammadrizki@gmail.com>
2026-06-12 20:05:27 +0700

    Fix: Increase minSdk to 26 for ZegoCloud and remove deprecated package attribute in Manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.rproject.chitchat"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
